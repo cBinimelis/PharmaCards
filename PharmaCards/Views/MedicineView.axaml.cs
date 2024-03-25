132c4ec99e4ed37f457b5b79f7a739a7e0494ba8
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-03-25 20:34:40 -0300

    Main Logo

---
+++ PharmaCards/Views/MedicineView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PharmaCards;
+
+public partial class MedicineView : UserControl
+{
+    public MedicineView()
+    {
+        InitializeComponent();
+    }
+}
